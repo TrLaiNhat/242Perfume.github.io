97ffd56bbf77f10db1d968789031ce99ee3820de
LẠI NHẬT TRƯỜNG <stansmith7012@gmail.com>
2025-03-13 08:47:00 +0000

    Mô tả ngắn gọn về các thay đổi

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ $searchQuery = isset($_GET['query']) ? trim($_GET['query']) : '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kết quả tìm kiếm</title>
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/page.css">
 </head>
 <body>
 
